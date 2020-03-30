a0e50eb568f57b38776465cfbe5e0353811092b3
rameshkumarvrs@gmail.com <rameshkumarvrs@gmail.com>
2020-03-30 19:59:58 +0530

    stock listing-views

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,8 @@
 class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
   def self.new_lookup(ticker)
     #client = IEX::Api::Client.new(publishable_token: 'Tpk_0b1b113041e34abebd0bb46362fcae8e',
                                   #secret_token: 'Tsk_0e977b9338fc41aa902328f60db29834',
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
 
-  validates :name, :ticker, presence: true
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
