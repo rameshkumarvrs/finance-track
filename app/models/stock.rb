cf94835f3e0a42b3c75e0d6a635e4c904db119aa
rameshkumarvrs@gmail.com <rameshkumarvrs@gmail.com>
2020-03-30 19:20:53 +0530

    added user-stocks model for many to many association

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,6 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
   def self.new_lookup(ticker)
     #client = IEX::Api::Client.new(publishable_token: 'Tpk_0b1b113041e34abebd0bb46362fcae8e',
                                   #secret_token: 'Tsk_0e977b9338fc41aa902328f60db29834',
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :user_stocks
+  has_many :stocks, through: :user_stocks
+
+  validates :name, :ticker, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
