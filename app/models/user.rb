961ad918c2e0441d5c54ee288e0c0b8e43be69ed
rameshkumarvrs@gmail.com <rameshkumarvrs@gmail.com>
2020-03-31 20:12:27 +0530

    stock tracing restrictions

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,19 @@ class User < ApplicationRecord
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+
+  def stock_already_tracked?(ticker)
+    stock = Stock.new_lookup(ticker)
+    return false unless stock
+    stocks.where(id: stock.id).exists?
+  end
+
+  def under_stock_limit?
+    stocks.count < 5
+  end
+
+  def can_track_stock?(ticker)
+    under_stock_limit? && !stock_already_tracked?(ticker)
+  end
 end
