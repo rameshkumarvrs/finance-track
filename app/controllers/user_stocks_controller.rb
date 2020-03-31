0a34ffcd87fcc87af38a5ddb25a9d07dc38c49ba
rameshkumarvrs@gmail.com <rameshkumarvrs@gmail.com>
2020-03-31 23:18:44 +0530

    remove stock functionality

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -9,4 +9,12 @@ class UserStocksController < ApplicationController
     flash[:notice] = "The #{stock.name} was added succesfully in your portfolio"
     redirect_to my_portfolio_path
   end  
+
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stock.destroy
+    flash[:notice] = "#{stock.ticker} was successfully deleted"
+    redirect_to my_portfolio_path
+  end
 end
