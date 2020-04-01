f5ec2b1d8346d51123e4e04f4661ebad94508ef7
rameshkumarvrs@gmail.com <rameshkumarvrs@gmail.com>
2020-04-01 17:36:32 +0530

    added user friends functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,33 @@ class UsersController < ApplicationController
   def my_portfolio
     @tracked_stocks = current_user.stocks
   end
+
+  def my_friends
+  	@friends = current_user.friends
+  end
+
+  def search
+  	#render json: params[:friend]
+    if params[:friend].present?
+    	@friends = User.search(params[:friend])
+    	@friends = current_user.except_current_user(@friends)
+      if @friends
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' }
+        end  
+        
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Could n't find user"
+          format.js { render partial: 'users/friend_result'}
+        end
+      end  
+    else
+      
+      respond_to do |format|
+          flash.now[:alert] = "Please Enter the Friend name to Search"
+          format.js { render partial: 'users/friend_result'}
+      end
+    end  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'my_friends', to: 'users#my_friends'
+  get 'search_friend', to: 'users#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
